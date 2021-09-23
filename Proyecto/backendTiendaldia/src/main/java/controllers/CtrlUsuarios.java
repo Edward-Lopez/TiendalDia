86b98180ce1fb03e79e403abb34fb4bb493508b8
Haresca <thehespway@gmail.com>
2021-09-23 12:27:00 -0500

    Update CtrlUsuarios.java

--- Proyecto/backendTiendaldia/src/main/java/controllers/CtrlUsuarios.java
+++ Proyecto/backendTiendaldia/src/main/java/controllers/CtrlUsuarios.java
@@ -1,10 +1,22 @@
 
 package controllers;
 
+import DAO.DAOUsuarios;
+import TO.TOUsuarios;
+import java.util.ArrayList;
+
 /**
  *
  * @author Personal
  */
 public class CtrlUsuarios {
+    DAOUsuarios usuariosDAO;
+
+    public CtrlUsuarios() {
+        this.usuariosDAO = new DAOUsuarios();
+    }
     
+    public ArrayList<TOUsuarios> consultarUsuarios(){
+        return usuariosDAO.consultar(nombreTabla);
+    }
 }

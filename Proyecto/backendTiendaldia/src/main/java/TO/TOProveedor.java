055e397c6fa84ed93c68f43f27f451bdf4fcaece
dhmarins <dhmarins@gmail.com>
2021-09-22 19:22:59 -0500

    Update TOProveedor.java

--- Proyecto/backendTiendaldia/src/main/java/TO/TOProveedor.java
+++ Proyecto/backendTiendaldia/src/main/java/TO/TOProveedor.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package TO;
 
-/**
- *
- * @author Hogar
- */
-public class TOProveedor {
+
+public class TOProveedor extends TOUsuarios{
+    private int idProveedor;
+    private String nombreProveedor;
+    private int idUsuario;
+
+    public TOProveedor() {
+        
+    }     
     
+    public int getIdProveedor() {
+        return idProveedor;
+    }
+
+    public String getNombreProveedor() {
+        return nombreProveedor;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdProveedor(int idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public void setNombreProveedor(String nombreProveedor) {
+        this.nombreProveedor = nombreProveedor;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
 }
